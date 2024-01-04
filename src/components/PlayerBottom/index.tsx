6afd14c43e805267d127566a298c009c5fee5669
Giovane Souza <giovane.souza@irroba.com.br>
2024-01-04 12:10:35 -0300

    Add player bottom

---
+++ src/components/PlayerBottom/index.tsx
@@ -0,0 +1,7 @@
+export function PlayerBottom() {
+    return(
+        <section className="border-t-2 h-20 border-gray-100">
+            <span className="">Player Bottom</span>
+        </section>
+    );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/global.scss'
 
 import { Header } from '../components/Header';
+import { PlayerBottom } from '../components/PlayerBottom'
 
 import { PlayerContextProvider } from '../contexts/PlayerContext';
 
@@ -8,9 +9,10 @@ function MyApp({ Component, pageProps }) {
   return (
     <PlayerContextProvider>
       <Header />
-      <main className='flex'>
+      <main className='flex flex-col'>
         {/* <div className="flex-1"> */}
         <Component {...pageProps} />
+        <PlayerBottom />
         {/* </div> */}
       </main>
     </PlayerContextProvider>
