f52dc5c381d212110ceedcc979561a812d34a86c
Giovane Souza <giovane.souza@irroba.com.br>
2024-01-05 21:24:49 -0300

    Test url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 let baseURL = process.env.REACT_APP_API_BASE_URL;
 
+if (process.env.NODE_ENV == 'production') {
+    baseURL = 'https://8f7f-189-41-198-46.ngrok-free.app';
+}
+
 export const api = axios.create({
     baseURL: baseURL
 })
