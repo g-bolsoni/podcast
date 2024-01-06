059adbd8b7dcbb4e21a1b77a8c6d847a40ec897d
Giovane Souza <giovane.souza@irroba.com.br>
2024-01-06 00:22:04 -0300

    Update url

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 let baseURL = process.env.REACT_APP_API_BASE_URL;
 
 if (process.env.NODE_ENV == 'production') {
-    baseURL = 'https://8f7f-189-41-198-46.ngrok-free.app';
+    baseURL = 'https://api-podcast-dj5m.onrender.com/';
 }
 
 export const api = axios.create({
